08765071f2f78d60470f0add5bab6efeb66dd1d9
Twiorigin <cksdmlaka2050@gmail.com>
2025-09-10 21:50:34 +0900

    Return one track & add a new catalog entry to the database

--- Chapter07/cd_access.c
+++ Chapter07/cd_access.c
@@ -50,7 +50,7 @@ int database_initialize(const int new_database)
 void database_close(void)
 {
 	if (cdc_dbm_ptr) dbm_close(cdc_dbm_ptr);
-	if (cdt_dbm_ptr) dbm_close(cdt_dbm_ptr):
+	if (cdt_dbm_ptr) dbm_close(cdt_dbm_ptr);
 
 	cdc_dbm_ptr = cdt_dbm_ptr = NULL;
 }
@@ -81,3 +81,35 @@ cdc_entry get_cdc_entry(const char *cd_catalog_ptr)
 	}
 	return (entry_to_return);
 } /* get_cdc_entry */
+
+cdt_entry get_cdt_entry(const char *cd_catalog_ptr, const int track_no)
+{
+	cdt_entry entry_to_return;
+	char entry_to_find[CAT_CAT_LEN + 10];
+	datum local_data_datum;
+	datum local_key_datum;
+
+	memset(&entry_to_return, '\0', sizeof(entry_to_return));
+
+	if (!cdc_dbm_ptr || !cdt_dbm_ptr) return (entry_to_return);
+	if (!cd_catalog_ptr) return (entry_to_return);
+	if (strlen(cd_catalog_ptr) >= CAT_CAT_LEN) return (entry_to_return);
+	/* Create a search key composed of the catalog entry and the track number. */
+	memset(&entry_to_find, '\0', sizeof(entry_to_find));
+	sprintf(entry_to_find, "%s %d", cd_catalog_ptr, track_no);
+
+	local_key_datum.dptr = (void *) entry_to_find;
+	local_key_datum.dsize = sizeof(entry_to_find);
+
+	memset(&local_data_datum, '\0', sizeof(local_data_datum));
+	local_data_datum = dbm_fetch(cdt_dbm_ptr, local_key_datum);
+	if (local_data_datum.dptr) {
+		memcpy(&entry_to_return, (char *) local_data_datum.dptr, local_data_datum.dsize);
+	}
+	return (entry_to_return);
+}
+
+int add_cdc_entry(const cdc_entry entry_to_add)
+{
+	char key_to_add[CAT_CAT_LEN + 1];
+
